6ccce3abe25f68d2b47f795a381285227967362e
Evgeniy_Petrovskiy <work.matwell@gmail.com>
2019-12-13 10:49:19 +0300

    Rework

--- 1_ishop/index.js
+++ 1_ishop/index.js
@@ -1,30 +1,3 @@
-var shopName ='Очень красивое название для магазина';
-var productArr = [
-    {name:'Материнская плата Gigabyte',
-    prise:150,
-    foto:'https://shop.by/images/gigabyte_z390_aorus_pro_wifi_%28rev__1_0%29_1.jpg',
-    inStock:8},
-    {name:'Оперативная память Kingston DDR4 Hyper-X 8Gb',
-    prise:100,
-    foto:'https://www.onlinetrade.ru/img/items/m/910818_1.jpg',
-    inStock:16},
-    {name:'Процессор AMD Ryzen 5 2600',
-    prise:230,
-    foto:'https://avatars.mds.yandex.net/get-mpic/1363071/img_id6895287055628929609.jpeg/9hq',
-    inStock:8},
-    {name:'Видеокарта MSI GeForce GTX 1050Ti',
-    prise:300,
-    foto:'https://avatars.mds.yandex.net/get-mpic/199079/img_id8541849599035302376/9hq',
-    inStock:8},
-    {name:'Блок питания beQuiet SP11 750W',
-    prise:320,
-    foto:'https://content2.onliner.by/catalog/device/header@2/28a8d600cdb8caf50f82fd4e0eb44b2c.jpeg',
-    inStock:8},
-    {name:'Корпус Zalman K1',
-    prise:165,
-    foto:'https://content2.onliner.by/catalog/device/200x200/4181dd4e15da5eda5cb16953a964b97c.jpeg',
-    inStock:8},
-]
 var ProductBlock = React.createClass({
 
     displayName:'ProductBlock',
@@ -33,20 +6,37 @@ var ProductBlock = React.createClass({
         return { inStock: "Нет на складе" }
     },
     render: function(){
+
+        var headerCode=[];
+        for(var n=0; n<this.props.tHeader.lenght; n++){
+            var thElement = this.props.tHeader[n];
+            var headerCode =
+            React.DOM.tr({key:thElement.nameH,className:'TableHeader'},   
+                React.DOM.td(thElement.nameH),
+                React.DOM.td(thElement.priseH),             
+                React.DOM.td(thElement.inStockH),
+                React.DOM.td(thElement.fotoH), 
+            );
+            headerCode.push(headerCode);
+        }
         var productsCode = [];
         for(var i=0; i < this.props.card.length; i++ ){
             var product = this.props.card[i];
             var productCode=
-            React.DOM.div({key:product.name,className:'Product'},
-                React.DOM.div({className:'Foto'},product.foto),    
-                React.DOM.div({className:'Name'},product.name),
-                React.DOM.div({className:'Prise'},product.prise),             
-                React.DOM.div({className:'InStock'},product.inStock),
+            React.DOM.tr({key:product.name,className:'Product'},   
+                React.DOM.td({className:'Name'},product.name),
+                React.DOM.td({className:'Prise'},product.prise),             
+                React.DOM.td({className:'InStock'},product.inStock),
+                React.DOM.td({className:'Foto'},product.foto), 
             );
             productsCode.push(productCode); 
         }
         return React.DOM.div({className:'ProductBlock'},
-        React.DOM.div({className:'Products'},productsCode),
+            React.DOM.span({className:'ShopTitle'},this.props.shopTitle),
+            React.DOM.table({className:'ProductTable'},
+                React.DOM.thead({className:'TableHdr'},headerCode),
+                React.DOM.tbody({className:'Products'},productsCode),
+            ),
         );
     },
 })
